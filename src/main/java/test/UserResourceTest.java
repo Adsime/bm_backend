dc24a60a4760f845f3abf5e2a66128913e579bb7
Adrian <melsom.adrian@accenture.com>
2017-02-03 17:59:02 +0100

    Minor edits

--- src/main/java/test/UserResourceTest.java
+++ src/main/java/test/UserResourceTest.java
@@ -1,7 +1,37 @@
 package test;
 
+import org.junit.Test;
+
 /**
  * Created by melsom.adrian on 03.02.2017.
  */
 public class UserResourceTest extends MockitoResource {
+
+    @Test
+    public void getUserTest() {
+        
+    }
+
+    @Test
+    public void getAllUsersTest() {
+
+    }
+
+    @Test
+    public void newUserTest() {
+
+    }
+
+    @Test
+    public void deleteUserTest() {
+
+    }
+
+    @Test
+    public void updateUser() {
+
+    }
+
+
+
 }
