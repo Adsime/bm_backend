ee4b34a7103478251e67e2d780d4d41dc5f16294
Adrian <melsom.adrian@accenture.com>
2017-02-14 14:45:14 +0100

    Wrote the UserResource class. Also added the models back in to the project

---
+++ src/main/java/com/acc/models/Tag.java
@@ -0,0 +1,7 @@
+package com.acc.models;
+
+/**
+ * Created by melsom.adrian on 14.02.2017.
+ */
+public class Tag {
+}
--- src/main/java/com/acc/resources/UserResource.java
+++ src/main/java/com/acc/resources/UserResource.java
@@ -2,16 +2,17 @@ package com.acc.resources;
 
 import com.acc.controller.ProblemService;
 import com.acc.controller.UserService;
+import com.acc.database.pojo.User;
 import com.google.gson.Gson;
+import org.eclipse.jetty.http.HttpStatus;
 import org.junit.Before;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import javax.inject.Inject;
 import javax.json.JsonObject;
 import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.*;
+import java.util.List;
 
 /**
  * Created by melsom.adrian on 03.02.2017.
@@ -38,27 +39,58 @@ public class UserResource {
 
     @GET
     @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
     /**
      *
      */
     public Response getUser(@PathParam("id") int id, @Context HttpHeaders headers) {
-        throw new NotImplementedException();
+        if(service.verify(headers.getRequestHeader(HttpHeaders.AUTHORIZATION).get(0))) {
+            try {
+                User user = service.getUser(id);
+                if(user == null) {
+                    return Response.status(HttpStatus.BAD_REQUEST_400).build();
+                }
+                return Response.ok(user.toString()).build();
+            } catch(InternalServerErrorException isee) {
+                return Response.status(HttpStatus.INTERNAL_SERVER_ERROR_500).build();
+            }
+        } return Response.status(HttpStatus.UNAUTHORIZED_401).build();
     }
 
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
     /**
      *
      */
     public Response getAllUsers(@Context HttpHeaders headers) {
-        throw new NotImplementedException();
+        if(service.verify(headers.getRequestHeader(HttpHeaders.AUTHORIZATION).get(0))) {
+            try {
+                List<User> users = service.getAllUsers();
+                if(users == null || users.isEmpty()) {
+                    return Response.status(HttpStatus.BAD_REQUEST_400).build();
+                }
+                return Response.ok(users.toString()).build();
+            } catch(InternalServerErrorException isee) {
+                return Response.status(HttpStatus.INTERNAL_SERVER_ERROR_500).build();
+            }
+        } return Response.status(HttpStatus.UNAUTHORIZED_401).build();
     }
 
     @POST
+    @Consumes(MediaType.APPLICATION_JSON)
     /**
      *
      */
     public Response newUser(JsonObject o, @Context HttpHeaders headers) {
-        throw new NotImplementedException();
+        if(service.verify(headers.getRequestHeader(HttpHeaders.AUTHORIZATION).get(0))) {
+            try {
+                if(service.newUser(new Gson().fromJson(o.toString(), User.class))) {
+                    return Response.ok().build();
+                }
+            } catch(InternalServerErrorException isee) {
+                return Response.status(HttpStatus.INTERNAL_SERVER_ERROR_500).build();
+            }
+        } return Response.status(HttpStatus.UNAUTHORIZED_401).build();
     }
 
     @DELETE
@@ -67,7 +99,16 @@ public class UserResource {
      *
      */
     public Response deleteUser(@PathParam("id") int id, @Context HttpHeaders headers) {
-        throw new NotImplementedException();
+        if(service.verify(headers.getRequestHeader(HttpHeaders.AUTHORIZATION).get(0))) {
+            try {
+                if(service.deleteUser(id)) {
+                    return Response.ok().build();
+                }
+                return Response.status(HttpStatus.BAD_REQUEST_400).build();
+            } catch(InternalServerErrorException isee) {
+                return Response.status(HttpStatus.INTERNAL_SERVER_ERROR_500).build();
+            }
+        } return Response.status(HttpStatus.UNAUTHORIZED_401).build();
     }
 
     @PUT
@@ -75,6 +116,15 @@ public class UserResource {
      *
      */
     public Response updateUser(JsonObject o, @Context HttpHeaders headers) {
-        throw new NotImplementedException();
+        if(service.verify(headers.getRequestHeader(HttpHeaders.AUTHORIZATION).get(0))) {
+            try {
+                if(service.updateUser(new Gson().fromJson(o.toString(), User.class))) {
+                    return Response.ok().build();
+                }
+                return Response.status(HttpStatus.BAD_REQUEST_400).build();
+            } catch(InternalServerErrorException isee) {
+                return Response.status(HttpStatus.INTERNAL_SERVER_ERROR_500).build();
+            }
+        } return Response.status(HttpStatus.UNAUTHORIZED_401).build();
     }
 }
