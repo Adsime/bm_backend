6f82064db23eb39bb5e16c75660831393a32d5db
Khac <nguyen.duy.j.khac@accenture.com>
2017-02-15 15:28:48 +0100

    repository implementation

---
+++ src/main/java/com/acc/database/repository/Specification.java
@@ -0,0 +1,7 @@
+package com.acc.database.repository;
+
+/**
+ * Created by nguyen.duy.j.khac on 15.02.2017.
+ */
+public interface Specification {
+}
---
+++ src/main/java/com/acc/database/repository/TagSpecification.java
@@ -0,0 +1,8 @@
+package com.acc.database.repository;
+
+/**
+ * Created by nguyen.duy.j.khac on 14.02.2017.
+ */
+public class TagSpecification {
+    TagSpecification(){}
+}
