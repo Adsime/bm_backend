9521095c03de84da21e1ffe41785361dfc84dcb7
Adsime <adrianmelsom@gmail.com>
2017-03-01 14:23:00 +0100

    Added some tests for userService. Also wrote the code for the tests

--- src/main/java/com/acc/service/UserService.java
+++ src/main/java/com/acc/service/UserService.java
@@ -6,26 +6,43 @@ import com.acc.database.specification.GetUserByIdSpec;
 import com.acc.database.specification.Specification;
 import com.acc.models.User;
 
+import javax.ejb.NoSuchEntityException;
 import javax.inject.Inject;
+import javax.ws.rs.InternalServerErrorException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 /**
  * Created by melsom.adrian on 10.02.2017.
  */
 public class UserService extends GeneralService {
 
+
     @Inject
     public UserRepository userRepository;
 
+    public UserService() {
 
-
-    public User getUser(int id) {
-        List<User> users = userRepository.getQuery(new GetUserByIdSpec(id));
-        return users.isEmpty() ? null : users.get(0);
     }
 
-    public List<User> getAllUsers() throws Exception{
-        return userRepository.getQuery(new GetUserAllSpec());
+    public User getUser(int id) throws InternalServerErrorException {
+        try {
+            List<User> users = userRepository.getQuery(new GetUserByIdSpec(id));
+            return users.isEmpty() ? null : users.get(0);
+        } catch (NoSuchEntityException nsee) {
+            return null;
+        }
+
+    }
+
+    public List<User> getAllUsers() throws InternalServerErrorException {
+        try {
+            return userRepository.getQuery(new GetUserAllSpec());
+        } catch (NoSuchEntityException nsee) {
+            return Arrays.asList();
+        }
     }
 
     public User newUser(User user) throws Exception {
--- src/test/main/java/com/acc/UserServiceTest.java
+++ src/test/main/java/com/acc/UserServiceTest.java
@@ -1,7 +1,100 @@
 package main.java.com.acc;
 
+import com.acc.database.repository.UserRepository;
+import com.acc.database.specification.GetUserByIdSpec;
+import com.acc.models.User;
+import com.acc.service.UserService;
+import main.java.com.acc.testResources.TestData;
+import net.bytebuddy.implementation.bytecode.Throw;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import javax.ejb.NoSuchEntityException;
+import javax.inject.Inject;
+import javax.ws.rs.InternalServerErrorException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
 /**
  * Created by melsom.adrian on 14.02.2017.
  */
 public class UserServiceTest {
+
+    @Mock
+    UserRepository userRepository;
+
+    UserService service;
+
+
+
+    @Before
+    public void setup() {
+        initMocks(this);
+        service = new UserService();
+        service.userRepository = userRepository;
+    }
+
+    //Start getUser()
+
+    @Test
+    public void getUserSuccessTest() {
+        when(userRepository.getQuery(any())).thenReturn(TestData.testUsers());
+        User expected = TestData.testUsers().get(0);
+        User actual = service.getUser(0);
+        assertEquals(expected.toString(), actual.toString());
+    }
+
+    @Test
+    public void getUserNoEntryException() {
+        when(userRepository.getQuery(any())).thenThrow(new NoSuchEntityException());
+        User expected = null;
+        User actual = service.getUser(0);
+        assertEquals(expected, actual);
+    }
+
+    @Test(expected = InternalServerErrorException.class)
+    public void getUserInternalError() {
+        when(userRepository.getQuery(any())).thenThrow(new InternalServerErrorException());
+        service.getUser(0);
+    }
+
+    //End getUser()
+    //Start getAllUsers()
+
+    @Test
+    public void getAllUsersSuccess() {
+        when(userRepository.getQuery(any())).thenReturn(TestData.testUsers());
+        String expected = TestData.testUsers().toString();
+        String actual = service.getAllUsers().toString();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getAllUsersEmptyList() {
+        when(userRepository.getQuery(any())).thenReturn(new ArrayList<User>());
+        boolean expected = service.getAllUsers().isEmpty();
+        assertTrue(expected);
+    }
+
+    @Test
+    public void getAllUsersNoSuchEntity() {
+        when(userRepository.getQuery(any())).thenThrow(new NoSuchEntityException());
+        Object expected = Arrays.asList();
+        Object actual = service.getAllUsers();
+        assertEquals(expected, actual);
+    }
+
+    @Test(expected = InternalServerErrorException.class)
+    public void getAllUsersInternalError() {
+        when(userRepository.getQuery(any())).thenThrow(new InternalServerErrorException());
+        service.getAllUsers();
+    }
 }
