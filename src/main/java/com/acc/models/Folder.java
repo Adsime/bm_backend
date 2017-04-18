e489e25462e5836e4f3f23a77ffb2184294a0632
Adsime <adrianmelsom@gmail.com>
2017-04-18 10:42:06 +0200

    For merge

--- src/main/java/com/acc/models/Folder.java
+++ src/main/java/com/acc/models/Folder.java
@@ -26,4 +26,12 @@ public class Folder {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "Folder{" +
+                "name: '" + name + '\'' +
+                ", parent:'" + parent + '\'' +
+                '}';
+    }
 }
